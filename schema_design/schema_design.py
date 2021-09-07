0b43c20c438c2dfc9ad6ddf4bf232500ab0f94be
fuodorov <fuodorov1998@gmail.com>
2021-09-07 13:15:07 +0700

    UPDATE schema_design.py

--- schema_design/schema_design.py
+++ schema_design/schema_design.py
@@ -4,26 +4,22 @@ import psycopg2
 
 if __name__ == "__main__":
     dsl = {
-        'dbname': os.getenv('DB_NAME', 'movies'),
-        'user': os.getenv('DB_USER', 'postgres'),
-        'host': os.getenv('DB_HOST', 'localhost'),
-        'port': os.getenv('DB_PORT', '5432'),
-        'password': os.getenv('DB_PASSWORD', 'postgres'),
+        'dbname': os.getenv('DB_NAME'),
+        'user': os.getenv('DB_USER'),
+        'host': os.getenv('DB_HOST'),
+        'port': os.getenv('DB_PORT'),
+        'password': os.getenv('DB_PASSWORD'),
     }
 
     SQL = """
     CREATE SCHEMA IF NOT EXISTS content;
     
-    CREATE TABLE IF NOT EXISTS content.genre (
-        id UUID PRIMARY KEY,
-        name TEXT NOT NULL,
-        description TEXT,
-        created TIMESTAMP WITH TIME ZONE,
-        modified TIMESTAMP WITH TIME ZONE
-    );
+    CREATE TYPE content.film_team_role AS ENUM ('director', 'writer', 'actor');
+
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
     CREATE TABLE IF NOT EXISTS content.film_work (
-        id UUID PRIMARY KEY,
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
         title TEXT NOT NULL,
         description TEXT,
         creation_date DATE,
@@ -31,35 +27,44 @@ if __name__ == "__main__":
         file_path TEXT,
         rating FLOAT,
         type TEXT NOT NULL,
-        created TIMESTAMP WITH TIME ZONE,
-        modified TIMESTAMP WITH TIME ZONE
+        created_at TIMESTAMP WITH TIME ZONE,
+        updated_at TIMESTAMP WITH TIME ZONE
     );
     
-    CREATE TABLE IF NOT EXISTS content.person (
-        id UUID PRIMARY KEY,
-        full_name TEXT NOT NULL,
-        birth_date DATE,
-        created TIMESTAMP WITH TIME ZONE,
-        modified TIMESTAMP WITH TIME ZONE
+    CREATE TABLE IF NOT EXISTS content.genre (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        name TEXT NOT NULL UNIQUE,
+        description TEXT,
+        created_at TIMESTAMP WITH TIME ZONE,
+        updated_at TIMESTAMP WITH TIME ZONE
     );
     
     CREATE TABLE IF NOT EXISTS content.genre_film_work (
-        id UUID PRIMARY KEY,
-        film_work_id UUID NOT NULL,
-        genre_id UUID NOT NULL,
-        created TIMESTAMP WITH TIME ZONE
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        film_work_id UUID REFERENCES content.film_work (id) NOT NULL,
+        genre_id UUID REFERENCES content.genre (id) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE
+    );
+    
+    CREATE TABLE IF NOT EXISTS content.person (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        full_name TEXT NOT NULL,
+        birth_date DATE,
+        created_at TIMESTAMP WITH TIME ZONE,
+        updated_at TIMESTAMP WITH TIME ZONE
     );
     
     CREATE TABLE IF NOT EXISTS content.person_film_work (
-        id UUID PRIMARY KEY,
-        film_work_id UUID NOT NULL,
-        person_id UUID NOT NULL,
-        role TEXT NOT NULL,
-        created TIMESTAMP WITH TIME ZONE
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        film_work_id UUID REFERENCES content.film_work (id) NOT NULL,
+        person_id UUID REFERENCES content.person (id) NOT NULL,
+        role content.film_team_role NOT NULL,
+        created_at TIMESTAMP with time zone
     );
-    
+
     CREATE UNIQUE INDEX film_work_genre ON content.genre_film_work (film_work_id, genre_id);
-    CREATE UNIQUE INDEX film_work_person_role ON content.person_film_work (film_work_id, person_id, role);
+    
+    CREATE UNIQUE INDEX film_work_person_role ON content.person_film_work (film_work_id, person_id, role);    
     """
 
     with psycopg2.connect(**dsl) as conn, conn.cursor() as cursor:
