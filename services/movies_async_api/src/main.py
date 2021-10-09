5088121b7a9718b174e5dedb262c8d6b47ebbcee
fuodorov <fuodorov1998@gmail.com>
2021-10-09 12:17:44 +0700

    Update

--- services/movies_async_api/src/main.py
+++ services/movies_async_api/src/main.py
@@ -1,5 +1,26 @@
 import uvicorn
 
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+
+app = FastAPI(
+    title="Movies Async API",
+    docs_url='/api/openapi',
+    openapi_url='/api/openapi.json',
+    default_response_class=ORJSONResponse,
+)
+
+
+@app.get("/api/hello")
+def read_root():
+    return {"Hello": "World"}
+
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000)
+    uvicorn.run(
+        'main:app',
+        host='0.0.0.0',
+        port=8000,
+        reload=True,
+        debug=True,
+    )
